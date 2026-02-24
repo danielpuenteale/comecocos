745500f6988f678d3d56b7b744d0830b0e6da5d7
Dani <puentealedaniel@fpllefia.com>
2026-02-24 12:35:18 +0100

    Add initial HTML structure and JavaScript for keyboard controls
    - Created index.html with a centered black box and a white square.
    - Added index.js to log key presses and update position variables for arrow keys.

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("Hola mundo");
+//detectar teclas
+document.addEventListener("keydown", controlteclas);
+function controlteclas(event) {
+  let tecla = event.key;
+  if (tecla == "ArrowUp") {
+    console.log("Arriba");
+    up = up - 5;
+    console.log(up);
+  }
+  if (tecla == "ArrowDown") {
+    console.log("Abajo");
+    down = down + 5;
+    console.log(down);
+  }
+    if (tecla == "ArrowLeft") { 
+      console.log("Izquierda");
+        left = left - 5;
+        console.log(left);
+    }
+    if (tecla == "ArrowRight") {
+      console.log("Derecha");
+        right = right + 5;
+        console.log(right);
+    }
+}
+
+let up = 0
+let left = 0
+let right = 0
+let down = 0
+
